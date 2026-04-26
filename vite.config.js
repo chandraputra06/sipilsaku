0d2e72a2bb8511470ec964bbe9852b064433c9a9
chandraputra06 <chandra.wisesa06@gmail.com>
2026-04-26 11:12:03 +0800

    fix: fixing bug css

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/ebook-carousel.js', 'resources/js/keunggulan-carousel.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/ebook-carousel.js', 'resources/js/keunggulan-carousel.js', 'resources/js/course-carousel.js'],
             refresh: true,
         }),
         tailwindcss(),
