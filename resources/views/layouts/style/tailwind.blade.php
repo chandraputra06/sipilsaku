4c89ee54f9bf24116c0013e224398a0ecfe2b139
chandraputra06 <chandra.wisesa06@gmail.com>
2026-04-26 10:50:53 +0800

    rename file

--- resources/views/layouts/style/tailwind.blade.php
+++ resources/views/layouts/style/tailwind.blade.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="{{ asset('build/assets/app-xxxxx.css') }}">
-<script type="module" src="{{ asset('build/assets/app-yyyyy.js') }}"></script>
-<script type="module" src="{{ asset('build/assets/ebook-carousel-zzzzz.js') }}"></script>
-<script type="module" src="{{ asset('build/assets/keunggulan-carousel-aaaaa.js') }}"></script>
-<script type="module" src="{{ asset('build/assets/course-carousel-zzzzz.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('build/assets/app-DcZ7yZK6.css') }}">
+<script type="module" src="{{ asset('build/assets/app-B5JitK_l.js') }}"></script>
+<script type="module" src="{{ asset('build/assets/ebook-carousel-DVlKG1hg.js') }}"></script>
+<script type="module" src="{{ asset('build/assets/keunggulan-carousel-CT5VZ7am.js') }}"></script>
+<script type="module" src="{{ asset('build/assets/course-carousel-7V06_1tN.js') }}"></script>
